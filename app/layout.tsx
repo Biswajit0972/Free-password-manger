49876c0fffe6e3838518cb80bc3f9bc6cc8c873d
Biswajit Das <workbiswajit0981@gmail.com>
2025-06-30 09:22:53 +0530

    update: Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import Navbar from "./_components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar/>
+       <main className="w-full relative">
+         {children}
+       </main>
       </body>
     </html>
   );
