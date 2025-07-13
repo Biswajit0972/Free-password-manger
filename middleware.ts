50e72115d4d89d73b20dea6ba1725b85b8c91d76
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-13 16:23:13 +0530

    add: middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,31 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
+const isPrivateRoute = createRouteMatcher(["/password", "/settings"]);
+
+export default clerkMiddleware(async (auth, req) => {
+    const {userId} = await auth();
+    const pathname = req.nextUrl.pathname;
+  
+  // ! if user is authenticated, also try ti access sign-in or sign-up page, redirect to home or prevent them.
+
+  if (userId && isPublicRoute(req)) {
+    console.log("preventing authenticated user from accessing public route");
+    return Response.redirect(new URL("/", req.url));
+  }
+
+  // ! if user is  unauthenticated, allow them to access private routes
+  if (!userId && isPrivateRoute(req)) {
+    console.log("not allowing authenticated user to access private route");
+    return Response.redirect(new URL("/sign-in", req.url));
+  }
+
+  
+}
+);
 
 export const config = {
+
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
