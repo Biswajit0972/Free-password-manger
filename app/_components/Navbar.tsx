549dac594602cf150683a5f30c32433a2839b892
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-13 16:20:26 +0530

    fix: add UserButton to Navbar for user session management and update Navigation component to use md breakpoint

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
-import {  useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Navigation from "./Navigation";
 import { authNavItems, navItems } from "../_utils";
 import Link from "next/link";
@@ -16,73 +16,84 @@ const Navbar = () => {
   const { isLoaded, isSignedIn } = useUser();
 
   return (
-    <div className="w-full h-18 bg-gray-500 px-5 py-2 relative flex-between ">
+    <div className="w-full h-18 bg-gray-500  px-5 py-2 relative flex-between">
       <Logo />
-      <div className="h-full relative flex-between  gap-2">
-    {isLoaded && isSignedIn ? (
-        <>
-          <Navigation
-            data={navItems}
-            render={(item) => {
-              return (
-                <Link
-                  href={item.href}
-                  key={item.href}
-                  className={`text-white  flex-center gap-2 group ${
-                    pathname === item.href ? "text-green-500 font-bold" : ""
-                  }`}
-                  title={item.title}
-                >
-                  <item.icon
-                    size={20}
-                    className={`  flex-center gap-2 group ${
+      {/* !  Navigation Items */}
+      <div className="hidden h-full md:max-w-[58%]  relative md:flex overflow-hidden">
+        <div className="h-full  w-full  relative flex-between gap-3">
+          {isLoaded && isSignedIn ? (
+            <Navigation
+              data={navItems}
+              render={(item) => {
+                return (
+                  <Link
+                    href={item.href}
+                    key={item.href}
+                    className={`text-white  flex-center gap-2 group ${
                       pathname === item.href ? "text-green-500 font-bold" : ""
-                    } group-hover:text-green-400 transition-all duration-300`}
-                  />
-                  <p className="text-sm group-hover:text-green-400 transition-all duration-300">
-                    {item.title}
-                  </p>
-                </Link>
-              );
-            }}
-          />
-        </>
-      ) : (
-       <>
-        <Navigation
-          data={authNavItems}
-          render={(item) => {
-            return (
-              <Link
-                href={item.href}
-                key={item.title}
-                className={`text-white  flex-center gap-2 group ${
-                  pathname === item.href ? "text-green-500 font-bold" : ""
-                }`}
-                title={item.title}
-              >
-                <item.icon
-                  size={20}
-                  className={`  flex-center gap-2 group ${
-                    pathname === item.href ? "text-green-500 " : ""
-                  } group-hover:text-green-400 transition-all duration-300`}
-                />
-                <p className="text-sm font-bold group-hover:text-green-400 transition-all duration-300">
-                  {item.title}
-                </p>
-              </Link>
-            );
-          }}
-        />
-       
-       </>
-      )}
+                    }`}
+                    title={item.title}
+                  >
+                    <item.icon
+                      size={20}
+                      className={`  flex-center gap-2 group ${
+                        pathname === item.href ? "text-green-500 font-bold" : ""
+                      } group-hover:text-green-400 transition-all duration-300`}
+                    />
+                    <p className="text-sm group-hover:text-green-400 transition-all duration-300">
+                      {item.title}
+                    </p>
+                  </Link>
+                );
+              }}
+            />
+          ) : (
+            <Navigation
+              data={authNavItems}
+              render={(item) => {
+                return (
+                  <Link
+                    href={item.href}
+                    key={item.title}
+                    className={`text-white  flex-center gap-2 group ${
+                      pathname === item.href ? "text-green-500 font-bold" : ""
+                    }`}
+                    title={item.title}
+                  >
+                    <item.icon
+                      size={20}
+                      className={`  flex-center gap-2 group ${
+                        pathname === item.href ? "text-green-500 " : ""
+                      } group-hover:text-green-400 transition-all duration-300`}
+                    />
+                    <p className="text-sm font-bold group-hover:text-green-400 transition-all duration-300">
+                      {item.title}
+                    </p>
+                  </Link>
+                );
+              }}
+            />
+          )}
+          {isLoaded && isSignedIn && (
+            <div className="h-10 w-10 bg-green-500 rounded-full flex-center ">
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: "w-10 h-10",
+                    userButtonAvatarImage: "w-10 h-10 rounded-full",
+                    userButtonAction: "hidden",
+                  },
+                }}
+              />
+            </div>
+          )}
+        </div>
       </div>
-  
-      <div className="h-full relative sm:hidden">
+
+      {/* !  Mobile Menu */}
+      <div className="h-full relative md:hidden">
         <MobileMenu />
       </div>
-      
     </div>
   );
 };
--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -8,7 +8,7 @@ const Navigation = <T,>({
   render: (data: T) => React.ReactNode;
 }) => {
   return (
-    <nav className="h-full hidden sm:flex items-center gap-5">
+    <nav className="h-full hidden md:flex items-center gap-5">
       {data && data.map(render)}
     </nav>
   );
