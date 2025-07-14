fe129e228e846e9a05199a7ce2b391a69d0720ed
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-15 01:25:44 +0530

    add: enhance Password component layout and styling

--- app/(private)/password/page.tsx
+++ app/(private)/password/page.tsx
@@ -1,8 +1,17 @@
+import { FolderPlus } from '@deemlol/next-icons'
 import React from 'react'
 
 const Password = () => {
   return (
-    <div>Password</div>
+    <div className='w-full h-full relative px-5 py-1 overflow-hidden flex-column'>
+      <h1 className='secondary-font text-center font-bold '>Password Store</h1>
+      <div className='w-full h-[calc(100%-2rem)]   bg-gray-200 p-1 rounded-lg'>
+      
+      </div>
+        <div className='h-15 w-15 absolute bottom-2 right-2 bg-green-500 rounded-full cursor-pointer flex-center hover:bg-green-600 transition-all duration-300 z-10'>
+        <FolderPlus size={24} color="#FFFFFF" />
+        </div>
+    </div>
   )
 }
 
