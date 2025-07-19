73a88bb47cab419491f9d35ea0825e29d699a29e
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-19 13:02:34 +0530

    feat: integrate PasswordForm component and toggle button in Password page

--- app/(private)/password/page.tsx
+++ app/(private)/password/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import CustomUl from "@/app/_components/CustomUl";
 import Dropdown from "@/app/_components/Dropdown";
 import PasswordForm from "@/app/_components/PasswordForm";
 import { fakeData } from "@/app/_utils";
 import { FolderPlus } from "@deemlol/next-icons";
-import React from "react";
+import React, { useState } from "react";
 
 const Password = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <div className="w-full h-full relative px-5 py-1 overflow-hidden flex-column">
       <h1 className="secondary-font text-center font-bold ">Password Store</h1>
@@ -21,12 +25,15 @@ const Password = () => {
             />
           )}
         />
-         <PasswordForm />
+        {open && <PasswordForm />}
       </div>
-      <div className="h-15 w-15 absolute bottom-2 right-2 bg-green-500 rounded-full cursor-pointer flex-center hover:bg-green-600 transition-all duration-300 z-10">
+
+      <button
+        className="h-15 w-15 absolute bottom-2 right-2 bg-green-500 rounded-full cursor-pointer flex-center hover:bg-green-600 transition-all duration-300 z-10"
+        onClick={() => setOpen(!open)}
+      >
         <FolderPlus size={24} color="#FFFFFF" />
-      </div>
-     
+      </button>
     </div>
   );
 };
