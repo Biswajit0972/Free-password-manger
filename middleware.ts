5128788df99207c209aa3c283f5db27049a891c7
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-19 09:18:31 +0530

    fix: correct response handling in middleware for route redirection

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
 const isPrivateRoute = createRouteMatcher(["/password", "/settings"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
- 
+
 
   // ! if user is authenticated, also try to access sign-in or sign-up page, redirect to home or prevent them.
 
   if (userId && isPublicRoute(req)) {
     console.log("preventing authenticated user from accessing public route");
-    return Response.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // ! if user is  unauthenticated, allow them to access private routes
+  // ! if user is  unauthenticated, not allow them to access private routes
   if (!userId && isPrivateRoute(req)) {
     console.log("not allowing authenticated user to access private route");
-    return Response.redirect(new URL("/sign-in", req.url));
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
 
