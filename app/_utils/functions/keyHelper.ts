845de34353a41c6fea5f814e1058823a08f72621
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-01 13:00:59 +0530

    feat: enhance key management functions with derived key generation and encryption methods

--- app/_utils/functions/keyHelper.ts
+++ app/_utils/functions/keyHelper.ts
@@ -16,7 +16,7 @@ export function arrayBufferToBase64(buffer: ArrayBuffer): string {
  * @returns {ArrayBuffer} The ArrayBuffer.
  * * atob is a built-in function in JavaScript that converts a base64 string to a binary string.
  */
-export function base64ToArrayBuffer(base64: string) {
+export function base64ToArrayBuffer(base64: string): Uint8Array<ArrayBuffer> {
     const binaryString = atob(base64);
     const length = binaryString.length;
     const bytes = new Uint8Array(length);
@@ -25,4 +25,100 @@ export function base64ToArrayBuffer(base64: string) {
         bytes[i] = binaryString.charCodeAt(i);
     }
     return bytes;
+}
+
+export const genBaseKey = async (masterPassword: string): Promise<CryptoKey> => {
+    const enc = new TextEncoder();
+    const stringToArray = enc.encode(masterPassword);
+    // ! encode convert string to Uint8Array
+    return await window.crypto.subtle.importKey("raw", stringToArray, "PBKDF2", true, ["deriveKey"]);
+}
+
+export const genDerivedKey = async (baseKey: CryptoKey, salt: Uint8Array<ArrayBuffer>): Promise<CryptoKey> => {
+    return await window.crypto.subtle.deriveKey(
+        {
+            name: "PBKDF2",
+            salt: salt,
+            iterations: 100000,
+            hash: "SHA-256",
+        },
+        baseKey,
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"]
+    );
+}
+
+// ? password and username encryption and decryption started here
+
+export const encryptData = async (data: string, key: CryptoKey, iv: Uint8Array<ArrayBuffer>): Promise<{ cipherText: string, iv: string }> => {
+    const enc = new TextEncoder();
+    const stringToArray = enc.encode(data);
+
+
+    const encryptedData = await window.crypto.subtle.encrypt({
+        name: "AES-GCM",
+        iv: iv,
+    },
+        key,
+        stringToArray
+    );
+
+    const encryptedBase64 = arrayBufferToBase64(encryptedData);
+    const ivBase64 = arrayBufferToBase64(iv.buffer);
+
+    return { cipherText: encryptedBase64, iv: ivBase64 };
+}
+
+export const decryptData = async (encryptedData: ArrayBuffer, key: CryptoKey, iv: Uint8Array<ArrayBuffer>): Promise<string> => {
+    const decryptedData = await window.crypto.subtle.decrypt({
+        name: "AES-GCM",
+        iv: iv,
+    },
+        key,
+        encryptedData
+    );
+
+    const decryptedString = new TextDecoder().decode(decryptedData);
+    return decryptedString;
+}
+
+// ? password and username encryption and decryption ended here
+
+// ** encryption of derived key started here
+
+export const encryptDerivedKey = async (baseKey: CryptoKey, derivedKey: CryptoKey, keyIv: Uint8Array<ArrayBuffer>): Promise<{ encryptedKey: string, iv: string }> => {
+    const rawDerivedKey = await window.crypto.subtle.exportKey("raw", derivedKey);
+
+    const encryptedKey = await window.crypto.subtle.encrypt({
+        name: "AES-GCM",
+        iv: keyIv
+    },
+        baseKey,
+        rawDerivedKey);
+
+    const encryptedKeyBase64 = arrayBufferToBase64(encryptedKey);
+    const base64KeyIv = arrayBufferToBase64(keyIv.buffer);
+
+    return { encryptedKey: encryptedKeyBase64, iv: base64KeyIv };
+}
+
+export const decryptDerivedKey = async (baseKey: CryptoKey, cipheredKey: Uint8Array<ArrayBuffer>, iv: Uint8Array<ArrayBuffer>) => {
+
+    const rawDerivedKey = await window.crypto.subtle.decrypt({
+        name: "AES-GCM",
+        iv: iv.buffer
+    },
+        baseKey,
+        cipheredKey.buffer);//     console.log(rawDerivedKey);
+
+    const derivedKey = await window.crypto.subtle.importKey(
+        "raw",
+        rawDerivedKey,
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"]
+    );
+   
+    return derivedKey;
 }
