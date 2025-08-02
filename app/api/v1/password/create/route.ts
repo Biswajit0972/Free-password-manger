4d2e055545bffc9a2d3241f795995848bf14409f
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-03 00:32:20 +0530

    feat: implement createPassword API endpoint for password creation

---
+++ app/api/v1/password/create/route.ts
@@ -0,0 +1,29 @@
+import { databaseConnection } from "@/app/_lib/db/database";
+import { PasswordModel } from "@/app/_lib/models/password/password.model";
+import { ApiResponse, ErrorResponse } from "@/app/_utils/functions/Apiresponse";
+import { AsyncHandler } from "@/app/_utils/functions/helper";
+import { NextRequest, NextResponse } from "next/server";
+
+async function createPassword(req: NextRequest) {
+    await databaseConnection();
+    const { user_id, username, password_obj, application_link } = await req.json();
+
+    if (!user_id || !username || !password_obj || !application_link) {
+        throw new ErrorResponse(400, "All fields are required");
+    }
+
+    const newPassword = await PasswordModel.create({
+        user_id,
+        username,
+        password_obj,
+        application_link,
+    });
+
+    if (!newPassword._id) {
+        throw new ErrorResponse(500, "Failed to create new password");
+    }
+
+    return NextResponse.json(new ApiResponse(201, "Password created successfully"), { status: 201 });
+}
+
+export const POST = AsyncHandler(createPassword);
