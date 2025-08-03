9089a0879cd24e21f9013f8d67c9dcb5a0c79814
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-03 13:22:31 +0530

    feat: refactor getAllPasswords function to use aggregation for password retrieval and improve data structure

--- app/api/v1/password/getPassword/route.ts
+++ app/api/v1/password/getPassword/route.ts
@@ -15,14 +15,45 @@ async function getAllPasswords(req: NextRequest) {
         throw new ErrorResponse(400, "user_id is required in query params");
     }
 
-    const user = await UserModel.findOne({clerkId: user_id});
+    const user = await UserModel.findOne({ clerkId: user_id });
 
     if (!user) {
         throw new ErrorResponse(404, "User not found");
     }
-     
-    
-    const passwords = await PasswordModel.find({ user_id: user._id });
+
+
+    const passwords = await PasswordModel.aggregate([
+        {
+            $match: {
+                user_id: "688dc53c2a04dff5a444538a"
+            }
+        },
+        {
+            $group: {
+                _id: "$application_link",
+                user_id: { $first: "$user_id" },
+                accounts: {
+                    $push: {
+                        username: "$username",
+                        password: "$password_obj.password",
+                        password_id: "$_id"
+                    }
+                }
+            }
+        },
+        {
+            $addFields: {
+                sitename: "$_id",
+            }
+        },
+        {
+            $project: {
+                user_id: 1,
+                accounts: 1,
+                sitename: 1
+            }
+        }
+    ]);
 
     if (!passwords || passwords.length === 0) {
         throw new ErrorResponse(404, "No passwords found for this user");
