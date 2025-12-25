c1c4593e6624ea4b6624c76b00e029ffda645f1b
roni <codingpro0981@gmail.com>
2025-12-25 17:52:15 +0530

    update: added `deletePassword` function to handle password deletion in fetch utilities

--- app/_utils/functions/fetch/index.ts
+++ app/_utils/functions/fetch/index.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { Password } from "@/app/_lib/models/password/password.model";
+import {Password} from "@/app/_lib/models/password/password.model";
 
 export const fetchUserData = async (user_id: string) => {
     try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/getuser`, { user_id });
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/getuser`, {user_id});
         return response.data;
     } catch (error) {
         const err = error as Error;
@@ -34,5 +34,15 @@ export const fetchPasswords = async (user_id: string) => {
         const err = error as Error;
         console.error("Error fetching passwords:", err.message);
         return [];
+    }
+}
+
+export const deletePassword = async (password_id: string) => {
+    try {
+        const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/password/deletepassword`, {data: {password_id}});
+        return response.data;
+    } catch (error) {
+        const err = error as Error;
+        console.error("Error deleting password:", err.message);
     }
 }
