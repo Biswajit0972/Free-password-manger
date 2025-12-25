670eb1c9310ba6283588dced7384624800e4aa7a
roni <codingpro0981@gmail.com>
2025-12-25 17:40:11 +0530

    secure: added Clerk authentication to `deletepassword` route and updated environment variables

--- app/api/v1/password/deletepassword/route.ts
+++ app/api/v1/password/deletepassword/route.ts
@@ -1,20 +1,24 @@
 //! password id and user_id check both are matched then delete
 
-import { databaseConnection } from "@/app/_lib/db/database";
-import { PasswordModel } from "@/app/_lib/models/password/password.model";
-import { ApiResponse, ErrorResponse } from "@/app/_utils/functions/Apiresponse";
-import { AsyncHandler } from "@/app/_utils/functions/helper";
-import { NextRequest, NextResponse } from "next/server";
+import {databaseConnection} from "@/app/_lib/db/database";
+import {PasswordModel} from "@/app/_lib/models/password/password.model";
+import {ApiResponse, ErrorResponse} from "@/app/_utils/functions/Apiresponse";
+import {AsyncHandler} from "@/app/_utils/functions/helper";
+import {NextRequest, NextResponse} from "next/server";
+import {auth} from "@clerk/nextjs/server"
 
 async function deletePassword(req: NextRequest) {
     await databaseConnection();
+    const {isAuthenticated, userId, redirectToSignIn} = await auth();
+    if (!isAuthenticated) return redirectToSignIn()
+    const updatedId = userId.split("_")[1];
 
-    const { user_id, password_id } = await req.json() as { user_id: string, password_id: string };
+    const {password_id} = await req.json() as { password_id: string };
 
-    if (!user_id || !password_id) throw new ErrorResponse(400, "all fields are required");
+    if (!password_id) throw new ErrorResponse(400, "all fields are required");
 
     const deletePassword = await PasswordModel.deleteOne({
-        $and: [{ _id: password_id }, { user_id }]
+        $and: [{_id: password_id}, {user_id: updatedId}]
     });
 
     if (deletePassword) throw new ErrorResponse(500, "something wents , please try again later");
