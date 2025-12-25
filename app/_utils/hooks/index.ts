090d55d6b57e1190f1873622d8694bb7dbc70c4b
roni <codingpro0981@gmail.com>
2025-12-25 17:52:21 +0530

    update: added `useDeletePassword` hook for password deletion functionality

--- app/_utils/hooks/index.ts
+++ app/_utils/hooks/index.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
-import { createPassword, fetchUserData } from "../functions/fetch";
+import {createPassword, deletePassword, fetchUserData} from "../functions/fetch";
 import {  Password } from "@/app/_lib/models/password/password.model";
 import { queryClient } from "@/app/query/Provider";
 
@@ -38,6 +38,16 @@ export const useCreatePassword = () => {
     return useMutation({
         mutationKey: ["createPassword"],
         mutationFn: (data: Encryption) => createPassword(data.user_id, data.username, data.password_obj, data.application_link),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["passwords"] });
+        }
+    });
+}
+
+export const useDeletePassword = () => {
+    return useMutation({
+        mutationKey: ["deletePassword"],
+        mutationFn: (password_id: string) => deletePassword(password_id),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["passwords"] });
         }
