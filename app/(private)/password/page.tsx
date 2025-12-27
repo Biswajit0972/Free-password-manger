85117bd8902dbfb6537821cab230ff95430ac67e
roni <codingpro0981@gmail.com>
2025-12-27 10:15:14 +0530

    refactor: improve code readability and consistency in `password/page.tsx`, enhance error handling for password fetch

--- app/(private)/password/page.tsx
+++ app/(private)/password/page.tsx
@@ -2,73 +2,76 @@
 
 import CustomUl from "@/app/_components/CustomUl";
 import Dropdown from "@/app/_components/Dropdown";
-import { MasterPasswordPopup } from "@/app/_components/MasterPasswordPopup";
+import {MasterPasswordPopup} from "@/app/_components/MasterPasswordPopup";
 import PasswordForm from "@/app/_components/PasswordForm";
-import { useApplicationcontext } from "@/app/_context/Context";
-import { useCryptoContext } from "@/app/_context/CryptoProvider";
-import { SiteData } from "@/app/_utils";
-import { fetchPasswords } from "@/app/_utils/functions/fetch";
-import { useAuth } from "@clerk/nextjs";
-import { FolderPlus } from "@deemlol/next-icons";
-import { useQuery } from "@tanstack/react-query";
+import {useApplicationcontext} from "@/app/_context/Context";
+import {useCryptoContext} from "@/app/_context/CryptoProvider";
+import {SiteData} from "@/app/_utils";
+import {fetchPasswords} from "@/app/_utils/functions/fetch";
+import {useAuth} from "@clerk/nextjs";
+import {FolderPlus} from "@deemlol/next-icons";
+import {useQuery} from "@tanstack/react-query";
 
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
+
 
 const Password = () => {
-  const {state, dispatch} = useApplicationcontext();
-  const { derivedKey } = useCryptoContext();
-  const { userId } = useAuth();
+    const {state, dispatch} = useApplicationcontext();
+    const {derivedKey} = useCryptoContext();
+    const {userId} = useAuth();
 
-  const {
-    data: passwords,
-    isLoading,
-    error,
-  } = useQuery<SiteData[]>({
-    queryKey: ["passwords"],
-    queryFn: () => fetchPasswords(userId!.split("_")[1]),
-    enabled: !!userId,
-  });
+    const {
+        data: passwords,
+        isLoading,
+        error,
+    } = useQuery<SiteData[]>({
+        queryKey: ["passwords"],
+        queryFn: () => fetchPasswords(userId!.split("_")[1]),
+        enabled: !!userId,
+    });
 
-  if (!derivedKey) return <MasterPasswordPopup />;
+    if (!derivedKey) return <MasterPasswordPopup/>;
 
-  if (error) {
-    toast.error("Failed to fetch passwords. Please try again later.");
-  }
+    if (error) {
+        toast.error("Failed to fetch passwords. Please try again.");
+        return <div>Error fetching passwords</div>;
+    }
 
     return (
-    <div className="w-full h-full relative px-5  overflow-hidden flex-column md:flex  md:items-center md:justify-center bg-gray-700 overflow-y-auto">
-      <div className="w-full h-full relative md:w-1/2 ">
-        <h1 className="primary-font text-center font-bold py-3 text-gray-200">
-          Password Store
-        </h1>
-        <div className="w-full h-[calc(100%-2rem)]   bg-gray-200 p-1 rounded-lg relative">
-          {isLoading ? (
-            <div className="flex-center h-full">Loading...</div>
-          ) : passwords && passwords.length > 0 ? (
-            <CustomUl
-              data={passwords}
-              className="w-full h-full overflow-y-auto hide-scrollbar"
-              render={(password) => (
-                <Dropdown key={password.sitename} data={password} />
-              )}
-            />
-          ) : (
-            "No passwords found"
-          )}
-          {state.openForm && <PasswordForm/>}
-        </div>
+        <div
+            className="w-full h-full relative px-5  overflow-hidden flex-column md:flex  md:items-center md:justify-center bg-gray-700 overflow-y-auto">
+            <div className="w-full h-full relative md:w-1/2 ">
+                <h1 className="primary-font text-center font-bold py-3 text-gray-200">
+                    Password Store
+                </h1>
+                <div className="w-full h-[calc(100%-2rem)]   bg-gray-200 p-1 rounded-lg relative">
+                    {isLoading ? (
+                        <div className="flex-center h-full">Loading...</div>
+                    ) : passwords && passwords.length > 0 ? (
+                        <CustomUl
+                            data={passwords}
+                            className="w-full h-full overflow-y-auto hide-scrollbar"
+                            render={(password) => (
+                                <Dropdown key={password.sitename} data={password}/>
+                            )}
+                        />
+                    ) : (
+                        "No passwords found"
+                    )}
+                    {state.openForm && <PasswordForm/>}
+                </div>
 
-       {
-        !state.openForm &&  <button
-          className="h-15 w-15 absolute bottom-2 right-2 bg-green-500 rounded-full cursor-pointer flex-center hover:bg-green-600 transition-all duration-300 z-10"
-          onClick={() => dispatch({type:"TOGGLE_FORM"})}
-        >
-          <FolderPlus size={24} color="#FFFFFF" />
-        </button>
-       }
-      </div>
-    </div>
-  );
+                {
+                    !state.openForm && <button
+                        className="h-15 w-15 absolute bottom-2 right-2 bg-green-500 rounded-full cursor-pointer flex-center hover:bg-green-600 transition-all duration-300 z-10"
+                        onClick={() => dispatch({type: "TOGGLE_FORM"})}
+                    >
+                        <FolderPlus size={24} color="#FFFFFF"/>
+                    </button>
+                }
+            </div>
+        </div>
+    );
 };
 
 export default Password;
