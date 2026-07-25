4373750bd312bc56973fa23464a42512b9638bab
Biswajit <workbiswajit0981@gmail.com>
2026-07-25 21:20:47 +0530

    remove user_id from password.model.ts and add vaultId for  refrence.

--- app/_lib/models/password/password.model.ts
+++ app/_lib/models/password/password.model.ts
@@ -1,24 +1,29 @@
-import mongoose, { Document, Schema, Model } from 'mongoose';
+import mongoose, {Document, Schema, Model} from 'mongoose';
 import aggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
 export type Password = {
     password: string;
     iv: string;
 }
+
 export interface IPassword extends Document {
-    user_id: mongoose.Types.ObjectId;
+    vaultId: mongoose.Types.ObjectId;
     username: string;
     password_obj: Password;
     application_link: string;
 }
 
 const PasswordSchema: Schema<IPassword> = new Schema({
-    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    username: { type: String, required: true },
-    password_obj: {
-        password: { type: String, required: true },
-        iv: { type: String, required: true }
+    vaultId: {
+        type: mongoose.Types.ObjectId,
+        required: true,
     },
-    application_link: { type: String, required: true }
+    username: {type: String, required: true},
+    password_obj: {
+        password: {type: String, required: true},
+        iv: {type: String, required: true}
+    },
+    application_link: {type: String, required: true}
 });
 
 PasswordSchema.plugin(aggregatePaginate);
