78efa110f44a9ef7b0e17181d23427efc5654fca
Biswajit <workbiswajit0981@gmail.com>
2026-07-25 21:06:59 +0530

    Implement new dynamic route inside user. use params

---
+++ app/api/v1/user/[user_id]/route.ts
@@ -0,0 +1,26 @@
+import { databaseConnection } from '@/app/_lib/db/database';
+import { UserModel } from '@/app/_lib/models/user/user.model';
+import { ApiResponse, ErrorResponse } from '@/app/_utils/functions/Apiresponse';
+import { AsyncHandler } from '@/app/_utils/functions/helper';
+import { NextRequest, NextResponse } from 'next/server';
+
+
+async function getUser(req: NextRequest, ctx: RouteContext<"/api/v1/user/[user_id]">) {
+    const {user_id} = await ctx.params;
+
+    if (!user_id) {
+        return NextResponse.json({ error: 'Missing [user_id]' }, { status: 400 });
+    }
+
+    const user = await UserModel.findOne({ clerkId: user_id }).select(['-email', '-firstName', '-lastName', '-imageUrl', '-lastSignInAt', '-createdAt']);
+
+    if (!user) {
+        throw new ErrorResponse(404, 'User not found');
+    }
+
+    console.log(user)
+
+    return NextResponse.json(new ApiResponse(200, ``, 'success', ""), { status: 200 });
+}
+
+export const GET = AsyncHandler(getUser);
