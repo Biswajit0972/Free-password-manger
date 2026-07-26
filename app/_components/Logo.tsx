319cacf9d40b23a3edaab437db278dd3403b8723
Biswajit <workbiswajit0981@gmail.com>
2026-07-26 07:45:20 +0530

    fix background color and icon

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <Link href="/" className="cursor-pointer h-full w-14">
-      <div className=" h-full w-full relative bg-green-500 rounded-full flex-center">
-        <p className="text-2xl font-bold text-white">FP</p>
+    <Link href="/" className="cursor-pointer h-full w-15">
+      <div className=" h-full w-full relative bg-[#605B51] rounded-full flex-center">
+        <p className="text-2xl font-bold text-white">
+            🔒
+        </p>
       </div>
     </Link>
   );
