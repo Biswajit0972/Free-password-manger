4c3ec375a8f7f38d834a1c9732a478f5e42d74dc
Biswajit <workbiswajit0981@gmail.com>
2026-07-27 19:21:49 +0530

    Add parameter vault_id inside create password function

--- app/_utils/functions/fetch/index.ts
+++ app/_utils/functions/fetch/index.ts
@@ -20,19 +20,15 @@ export const fetchUserData = async (user_id: string) => {
     }
 }
 
-export const createPassword = async (user_id: string, username: string, password_obj: Password, application_link: string) => {
-    try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/password/create`, {
-            user_id,
-            username,
-            password_obj,
-            application_link
-        });
-        return response.data;
-    } catch (error) {
-        const err = error as Error;
-        console.error("Error creating password:", err.message);
-    }
+export const createPassword = async (user_id: string, username: string, password_obj: Password, application_link: string, vault_id:string) => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/password/create`, {
+        vault_id,
+        user_id,
+        username,
+        password_obj,
+        application_link
+    });
+    return response.data?.data;
 }
 
 export const fetchPasswords = async (user_id: string) => {
