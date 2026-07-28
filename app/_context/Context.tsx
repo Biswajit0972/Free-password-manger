bc5580fd5736c99f41fe17241972cd8b2595abc3
Biswajit <workbiswajit0981@gmail.com>
2026-07-28 13:10:04 +0530

    Add extra field vaultId and action

--- app/_context/Context.tsx
+++ app/_context/Context.tsx
@@ -5,68 +5,70 @@ import React, {
   useReducer,
   ReactNode,
   Dispatch,
-  useContext,
+  useContext, useEffect,
 } from "react";
+import {useGetUserData} from "@/app/_utils/hooks";
 
 type StateType = {
-  password: string;
-  openForm: boolean;
+    password: string;
+    openForm: boolean;
+    vaultId: string;
 };
 
-type Actions =
-  | {
-      type: "ADD_PASSWORD";
-      payload: string;
-    }
-  | {
-      type: "TOGGLE_FORM";
-    };
+type Actions = { type: "ADD_PASSWORD"; payload: string; } | { type: "TOGGLE_FORM"; } | {
+    type: "CHOOSE_VAULT",
+    payload: string
+};
 
 type ContextType = {
-  state: StateType;
-  dispatch: Dispatch<Actions>;
+    state: StateType;
+    dispatch: Dispatch<Actions>;
 };
 
 const initialState: StateType = {
-  password: "",
-  openForm: false,
+    password: "",
+    openForm: false,
+    vaultId: ""
 };
 
 const PasswordContext = createContext<ContextType>({
-  state: initialState,
-  dispatch: () => {},
+    state: initialState,
+    dispatch: () => {
+    },
 });
 
 function reducer(state: StateType, action: Actions): StateType {
-  switch (action.type) {
-    case "ADD_PASSWORD":
-      return { ...state, password: action.payload, openForm: true };
-    case "TOGGLE_FORM":
-      return { ...state, openForm: !state.openForm };
-    default:
-      console.warn("Unknown action type");
-      return state;
-  }
+    switch (action.type) {
+        case "ADD_PASSWORD":
+            return {...state, password: action.payload, openForm: true};
+        case "TOGGLE_FORM":
+            return {...state, openForm: !state.openForm};
+        case "CHOOSE_VAULT":
+            return {...state, vaultId: action.payload};
+        default:
+            console.warn("Unknown action type");
+            return state;
+    }
 }
 
-const PasswordProvider = ({ children }: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+const PasswordProvider = ({children}: { children: ReactNode }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <PasswordContext.Provider value={{ state, dispatch }}>
-      {children}
-    </PasswordContext.Provider>
-  );
+    return (
+        <PasswordContext.Provider value={{state, dispatch}}>
+            {children}
+        </PasswordContext.Provider>
+    );
 };
 
 const useApplicationcontext = () => {
-  const context = useContext(PasswordContext);
-  if (context === undefined) {
-    throw new Error(
-      "useApplicationcontext must be used within a PasswordProvider"
-    );
-  }
-  return context;
+    const context = useContext(PasswordContext);
+    if (context === undefined) {
+        throw new Error(
+            "useApplicationcontext must be used within a PasswordProvider"
+        );
+    }
+    return context;
 };
 
-export { PasswordProvider, PasswordContext, useApplicationcontext };
+export {PasswordProvider, PasswordContext, useApplicationcontext};
