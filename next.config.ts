07d4970c894daa3987ceb7d1b675c8e284c7ff73
Biswajit <workbiswajit0981@gmail.com>
2026-07-28 20:32:24 +0530

    Update config. remove deprecated methods

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,57 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "www.google.com",
-      "www.facebook.com",
-      "twitter.com",
-      "github.com",
-      "www.linkedin.com",
-      "t1.gstatic.com"
-    ]
-  }
-  /* config options here */
+    images: {
+        remotePatterns: [
+            // Google profile images
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "t1.gstatic.com",
+            },
+
+            // GitHub avatars
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            },
+
+            // Facebook profile images
+            {
+                protocol: "https",
+                hostname: "platform-lookaside.fbsbx.com",
+            },
+
+            // LinkedIn profile images
+            {
+                protocol: "https",
+                hostname: "media.licdn.com",
+            },
+
+            // X (Twitter) profile images
+            {
+                protocol: "https",
+                hostname: "pbs.twimg.com",
+            },
+
+            {
+                protocol: "https",
+                hostname: "t1.gstatic.com",
+            },
+        ],
+    },
+
+    reactStrictMode: true,
+
+    experimental: {
+        optimizePackageImports: [
+            "@clerk/nextjs",
+            "@radix-ui/react-icons",
+        ],
+    },
 };
 
 export default nextConfig;
